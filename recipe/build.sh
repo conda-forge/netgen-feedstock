6364124f59fe4094a8533ebe584e2fe56e1c9f9b
John Kirkham <kirkhamj@janelia.hhmi.org>
2019-01-13 00:58:10 -0500

    Revert "Add `-undefined dynamic_lookup` to macOS `LDFLAGS`"
    This reverts commit 0615a452c14e6f084ac7f906ab612ab2d6825261.

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,6 @@ if [[ ${c_compiler} != "toolchain_c" ]]; then
     declare -a CMAKE_PLATFORM_FLAGS
     if [[ ${HOST} =~ .*darwin.* ]]; then
         CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
-        export LDFLAGS="${LDFLAGS} -undefined dynamic_lookup"
     else
         CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
     fi
