f446835cd7c3d1e71090a514b3bb39817fb414c5
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2019-01-14 14:14:57 +0000

    MNT: Re-rendered with conda-smithy 3.2.2 and pinning 2019.01.09

--- .circleci/build_steps.sh
+++ .circleci/build_steps.sh
@@ -34,7 +34,7 @@ source run_conda_forge_build_setup
 # "recipe/yum_requirements.txt" file. After updating that file,
 # run "conda smithy rerender" and this line will be updated
 # automatically.
-/usr/bin/sudo -n yum install -y mesa-libGL-devel
+/usr/bin/sudo -n yum install -y libXt-devel libXmu-devel libXi-devel mesa-libGL-devel
 
 
 # make the build number clobber
