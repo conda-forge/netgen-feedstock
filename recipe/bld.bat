2255fec5d52e49927594b42e2a14838c99c75dda
looooo <sppedflyer@gmail.com>
2019-01-24 22:27:59 +0100

    add option to disable dynamic linking of python

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,19 +3,21 @@ cd build
 
 REM TODO: can not find tkint.h -> disabling GUI for WIN
 
-cmake .. -G "Ninja" ^
-    -DCMAKE_BUILD_TYPE="Release" ^
-    -DINSTALL_DIR_LAYOUT="Unix" ^
-    -DCMAKE_PREFIX_PATH:FILEPATH="%PREFIX%" ^
-    -DCMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%" ^
-    -DNG_INSTALL_DIR_INCLUDE:FILEPATH="%LIBRARY_PREFIX%/include/netgen" ^
-    -DOCC_INCLUDE_DIR:FILEPATH="%LIBRARY_PREFIX%/include/opencascade" ^
-    -DOCC_LIBRARY_DIR:FILEPATH="%LIBRARY_PREFIX%/lib" ^
-    -DUSE_OCC=ON ^
-    -DUSE_PYTHON=ON ^
-    -DUSE_GUI=OFF ^
-    -DBUILD_WITH_CONDA=ON ^
-    -DUSE_SUPERBUILD=OFF
+cmake -G "Ninja" ^
+      -D CMAKE_BUILD_TYPE="Release" ^
+      -D INSTALL_DIR_LAYOUT="Unix" ^
+      -D CMAKE_PREFIX_PATH:FILEPATH="%PREFIX%" ^
+      -D CMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%" ^
+      -D NG_INSTALL_DIR_INCLUDE:FILEPATH="%LIBRARY_PREFIX%/include/netgen" ^
+      -D OCC_INCLUDE_DIR:FILEPATH="%LIBRARY_PREFIX%/include/opencascade" ^
+      -D OCC_LIBRARY_DIR:FILEPATH="%LIBRARY_PREFIX%/lib" ^
+      -D USE_OCC=ON ^
+      -D USE_PYTHON=ON ^
+      -D USE_GUI=OFF ^
+      -D BUILD_WITH_CONDA=ON ^
+      -D USE_SUPERBUILD=OFF ^
+      -D DYNAMIC_LINK_PYTHON=OFF ^
+      ..
 
 if errorlevel 1 exit 1
 ninja install
--- recipe/build.sh
+++ recipe/build.sh
@@ -10,23 +10,24 @@ if [[ ${c_compiler} != "toolchain_c" ]]; then
     fi
 fi
 
-cmake .. -G "Ninja" \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DNG_INSTALL_DIR_INCLUDE=$PREFIX/include/netgen \
-      -DNG_INSTALL_DIR_PYTHON=${SP_DIR} \
-      -DNG_INSTALL_DIR_BIN=bin \
-      -DNG_INSTALL_DIR_LIB=lib \
-      -DNG_INSTALL_DIR_CMAKE=lib/cmake/netgen \
-      -DNG_INSTALL_DIR_RES=share \
-      -DOCC_INCLUDE_DIR=$PREFIX/include/opencascade \
-      -DOCC_LIBRARY_DIR=$PREFIX/lib \
-      -DUSE_NATIVE_ARCH=OFF \
-      -DUSE_OCC=ON \
-      -DUSE_PYTHON=ON \
-      -DUSE_GUI=OFF \
-      -DUSE_SUPERBUILD=OFF \
-      -DBUILD_WITH_CONDA=ON \
+cmake -G "Ninja" \
+      -D CMAKE_BUILD_TYPE=Release \
+      -D CMAKE_INSTALL_PREFIX=$PREFIX \
+      -D NG_INSTALL_DIR_INCLUDE=$PREFIX/include/netgen \
+      -D NG_INSTALL_DIR_PYTHON=${SP_DIR} \
+      -D NG_INSTALL_DIR_BIN=bin \
+      -D NG_INSTALL_DIR_LIB=lib \
+      -D NG_INSTALL_DIR_CMAKE=lib/cmake/netgen \
+      -D NG_INSTALL_DIR_RES=share \
+      -D OCC_INCLUDE_DIR=$PREFIX/include/opencascade \
+      -D OCC_LIBRARY_DIR=$PREFIX/lib \
+      -D USE_NATIVE_ARCH=OFF \
+      -D USE_OCC=ON \
+      -D USE_PYTHON=ON \
+      -D USE_GUI=OFF \
+      -D USE_SUPERBUILD=OFF \
+      -D BUILD_WITH_CONDA=ON \
+      -D DYNAMIC_LINK_PYTHON=OFF \
       ${CMAKE_PLATFORM_FLAGS[@]} \
       ..
 
