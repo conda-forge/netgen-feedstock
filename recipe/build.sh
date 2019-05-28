31302a3b430aebd67a1410751730d1890cde01e7
looooo <sppedflyer@gmail.com>
2019-05-28 23:13:06 +0200

    remove unused line

--- recipe/build.sh
+++ recipe/build.sh
@@ -21,7 +21,6 @@ cmake -G "Ninja" \
       -D USE_SUPERBUILD=OFF \
       -D BUILD_WITH_CONDA=ON \
       -D DYNAMIC_LINK_PYTHON=OFF \
-      ${CMAKE_PLATFORM_FLAGS[@]} \
       ..
 
 ninja install -v
