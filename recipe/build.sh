6685fe310a5f03fae57597b774b4bdc9806d352b
AU <adam-urbanczyk@users.noreply.github.com>
2024-11-22 23:00:12 +0100

    Add PYTHON_MODULE_EXTENSION

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,8 +5,9 @@ cmake -G "Ninja" -B build \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
       -D CMAKE_PREFIX_PATH:FILEPATH=$PREFIX \
       -D Python3_FIND_STRATEGY=LOCATION \
-	-D Python3_ROOT_DIR=${PREFIX} \
+      -D Python3_ROOT_DIR=${PREFIX} \
       -D Python3_EXECUTABLE=$PYTHON \
+      -D PYTHON_MODULE_EXTENSION=`python -c 'import sysconfig; print(sysconfig.get_config_var("EXT_SUFFIX"))'` \
       -D NG_INSTALL_DIR_INCLUDE:FILEPATH=$PREFIX/include/netgen \
       -D NG_INSTALL_DIR_PYTHON:FILEPATH=$SP_DIR \
       -D NG_INSTALL_DIR_BIN=bin \
